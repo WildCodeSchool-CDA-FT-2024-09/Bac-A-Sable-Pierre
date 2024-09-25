c5bfd74e7492770fd0004d92a63ad9216ce4e3c2
pierre delarocque <delarocque.pierre@gmail.com>
2024-09-25 16:50:49 +0200

    creation get

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import router from "./router";
 
 const app = express();
+
+app.use(`/api`, router);
 
 app.listen(3000, async () => {
   console.log(`serveur is listenning on http://localhost:3000`);
