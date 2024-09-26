2323ebe675ad7e43fe4bc9d2bf991ade37d25567
pierre delarocque <delarocque.pierre@gmail.com>
2024-09-26 11:17:33 +0200

    creation post+validate

--- api/src/repos/repos.controllers.ts
+++ api/src/repos/repos.controllers.ts
@@ -1,8 +1,27 @@
-import express, { Response, Request } from "express";
+import express, { Response, Request, NextFunction } from "express";
+import Joi from "joi";
+
 import repos from "../../repos.json";
 import type { Repo } from "../repos/repo.type";
 
 const repoControllers = express.Router();
+
+const schema = Joi.object({
+  id: Joi.string().required(),
+  name: Joi.string().required(),
+  url: Joi.string().required(),
+  isPrivate: Joi.string().max(1).min(2).required(),
+});
+
+const validateRepo = (req: Request, res: Response, next: NextFunction) => {
+  const { error } = schema.validate(req.body);
+
+  if (error == null) {
+    next();
+  } else {
+    res.status(422).json(error);
+  }
+};
 
 repoControllers.get("/", (_: any, res: Response) => {
   res.status(200).json(repos);
@@ -16,6 +35,11 @@ repoControllers.get("/:id", (req: Request, res: Response) => {
   } else {
     res.sendStatus(404);
   }
+});
+
+repoControllers.post("/", validateRepo, (req: Request, res: Response) => {
+  repos.push(req.body);
+  res.status(201).json(req.body);
 });
 
 export default repoControllers;
