b0a1270ce56b5315c37c73d7f21d79544fb8a98b
pierre delarocque <delarocque.pierre@gmail.com>
2024-10-08 09:14:49 +0200

    flechter

--- github/src/pages/Detail.tsx
+++ github/src/pages/Detail.tsx
@@ -1,17 +1,41 @@
-import { useParams, useLoaderData } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import type { Repo } from "../types/RepoType";
+import { useEffect, useState } from "react";
+
+import connexion from "../services/connexion";
 
 export default function Detail() {
   console.log("Initialisation du Detail");
   const { id } = useParams();
-  const data = useLoaderData() as Repo;
+
+  const [data, setData] = useState<Repo | null>(null);
+
+  useEffect(() => {
+    console.log("I'm the useEffect");
+    const fetchRepos = async () => {
+      try {
+        const repos = await connexion.get<Repo[]>(`/api/repos/${id}`);
+        console.log(" useEffect repos", repos);
+        setData(repos.data[0]);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchRepos();
+  }, []);
 
   return (
     <>
       <div>Detail du Repo {id}</div>
       <div>
-        <h1>nom {data.name}</h1>
-        <p>{data.label}</p>
+        {data ? (
+          <>
+            <h1>nom {data.name}</h1>
+            <p>{data.label}</p>
+          </>
+        ) : (
+          <p>Loading...</p>
+        )}
       </div>
     </>
   );
